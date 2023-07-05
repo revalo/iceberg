bbdf7c3750ee962c85103071d732888dca61be22
Shreyas Kapur <shreyask@mit.edu>
2023-07-04 18:31:59 -0700

    Make renderer save fail if parent directory doesn't exist.

--- iceberg/core/renderer.py
+++ iceberg/core/renderer.py
@@ -6,6 +6,7 @@ from .properties import Color
 import skia
 import glfw
 import re
+import os
 
 import numpy as np
 
@@ -118,6 +119,13 @@ class Renderer(object):
         Args:
             path: The path to save the image to.
         """
+
+        path = Path(path)
+
+        if not path.parent.exists():
+            raise RuntimeError(
+                f"Destination directory {path.parent} does not exist. Please create it first."
+            )
 
         image = self._skia_surface.makeImageSnapshot()
         image.save(str(path))
