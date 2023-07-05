18718938b279e8fd9f5ba84e87c868cb576161f0
Shreyas Kapur <shreyask@mit.edu>
2023-07-04 18:10:42 -0700

    Output debug info.

--- tests/scene_tester.py
+++ tests/scene_tester.py
@@ -20,17 +20,17 @@ def check_render(
         AssertionError: If the rendered image does not match the expected image.
     """
 
-    expected_filename = os.path.join("tests", "testdata", expected_filename)
+    expected_full_filename = os.path.join("tests", "testdata", expected_filename)
 
     renderer = Renderer()
     renderer.render(drawable)
     rendered_image = renderer.get_rendered_image()
 
     if generate_expected:
-        renderer.save_rendered_image(expected_filename)
+        renderer.save_rendered_image(expected_full_filename)
         return
 
-    expected_image = Image.open(expected_filename)
+    expected_image = Image.open(expected_full_filename)
     rendered_image = Image.fromarray(rendered_image)
     img_diff = Image.new("RGBA", expected_image.size)
 
@@ -38,4 +38,10 @@ def check_render(
     percent_mismatch = mismatch / (expected_image.size[0] * expected_image.size[1])
 
     if percent_mismatch > 0.01:
+        # Save the rendered image, the expected image, and the diff image.
+        debug_dir = os.path.join("tests", "testoutput")
+        os.makedirs(debug_dir, exist_ok=True)
+        rendered_image.save(os.path.join(debug_dir, f"rendered_{expected_filename}"))
+        expected_image.save(os.path.join(debug_dir, f"expected_{expected_filename}"))
+        img_diff.save(os.path.join(debug_dir, f"diff_{expected_filename}"))
         assert False, f"Image mismatch: {percent_mismatch * 100:.2f}%"
